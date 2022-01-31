b167985f34284d8c681db93811faa115f68ef70d
nb950 <foth2012@gmail.com>
2022-01-31 12:13:44 -0500

    update message

--- controllers/csm_controller.go
+++ controllers/csm_controller.go
@@ -230,7 +230,7 @@ func (r *ContainerStorageModuleReconciler) handleDeploymentUpdate(oldObj interfa
 	d, _ := obj.(*appsv1.Deployment)
 	name := d.Spec.Template.Labels["csm"]
 	if name == "" {
-		r.Log.Info("deployment not found", "name", d.Name)
+		r.Log.Info("ignore deployment not labeled for csm", "name", d.Name)
 		return
 	}
 
@@ -308,7 +308,7 @@ func (r *ContainerStorageModuleReconciler) handleDaemonsetUpdate(oldObj interfac
 	d, _ := obj.(*appsv1.DaemonSet)
 	name := d.Spec.Template.Labels["csm"]
 	if name == "" {
-		r.Log.Info("daemonset not found", "name", d.Name)
+		r.Log.Info("ignore daemonset not labeled for csm", "name", d.Name)
 		return
 	}
 
