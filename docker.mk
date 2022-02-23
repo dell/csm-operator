2685d9927ba89096687df84b0badf4acff44fa82
Rensy Thomas <rensy.thomas@dell.com>
2022-02-23 19:09:37 +0530

    Add required manifests to install operator in OLM environment

--- docker.mk
+++ docker.mk
@@ -1,8 +1,18 @@
 # Registry for all images
 REGISTRY ?= dellemc
 
-# Default Operator image name
-OPERATOR_IMAGE ?= dell-csm-operator
+# IMAGE_TAG_BASE defines the docker.io namespace and part of the image name for remote images.
+# This variable is used to construct full image tags for bundle and catalog images.
+#
+# For example, running 'make bundle-build bundle-push catalog-build catalog-push' will build and push both
+# dell.com/csm-operator-bundle:$VERSION and dell.com/csm-operator-catalog:$VERSION.
+IMAGE_TAG_BASE ?= dell-csm-operator
+
+# Image tag base for community bundle images
+BUNDLE_IMAGE_TAG_BASE_COMMUNITY ?= dell-csm-community-operator-bundle
+
+# Image tag base for community catalog images
+CATALOG_IMAGE_TAG_BASE_COMMUNITY ?= dell-csm-community-operator-catalog
 
 # Operator version tagged with build number. For e.g. - v1.2.0.001
 VERSION ?= 0.1.0
@@ -12,5 +22,15 @@ TIMESTAMP := $(shell  date +%Y%m%d%H%M%S)
 
 # Image URL to use all building/pushing image targets
 # Local Image
-DEFAULT_IMG := "$(OPERATOR_IMAGE):$(TIMESTAMP)"
-IMG ?= "$(REGISTRY)/$(OPERATOR_IMAGE):$(VERSION)"
+DEFAULT_IMG ?= "$(IMAGE_TAG_BASE):$(TIMESTAMP)"
+
+# Operator image name
+IMG ?= "$(REGISTRY)/$(IMAGE_TAG_BASE):$(VERSION)"
+
+# OLM Images
+# BUNDLE_IMG defines the image:tag used for the bundle.
+# You can use it as an arg. (E.g make bundle-build BUNDLE_IMG=<some-registry>/<project-name-bundle>:<tag>)
+BUNDLE_IMG ?= "$(REGISTRY)/$(BUNDLE_IMAGE_TAG_BASE_COMMUNITY):$(VERSION)"
+
+# The image tag given to the resulting catalog image (e.g. make catalog-build CATALOG_IMG=example.com/operator-catalog:v0.2.0).
+CATALOG_IMG ?= "$(REGISTRY)/$(CATALOG_IMAGE_TAG_BASE_COMMUNITY):$(VERSION)"
