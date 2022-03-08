59d999924cf58ed8ff3824040016a367cd7facdf
Christian Coffield <christian_coffield@dell.com>
2022-03-08 12:32:45 -0500

    Minor change to clusterrole (#63)
    * Minor change to clusterrole
    
    * Update clusterrole.go
    
    * Update clusterrole.go
    
    * clusterole changes
    
    * Adding namespace kind
    
    * reverting back change

--- pkg/resources/rbac/clusterrole.go
+++ pkg/resources/rbac/clusterrole.go
@@ -11,7 +11,7 @@ import (
 )
 
 // SyncClusterRole - Syncs a ClusterRole
-func SyncClusterRole(ctx context.Context, clusterRole *rbacv1.ClusterRole, client client.Client) (*rbacv1.ClusterRole, error) {
+func SyncClusterRole(ctx context.Context, clusterRole *rbacv1.ClusterRole, client client.Client) error {
 	log := logger.GetLogger(ctx)
 	found := &rbacv1.ClusterRole{}
 	err := client.Get(ctx, types.NamespacedName{Name: clusterRole.Name, Namespace: clusterRole.Namespace}, found)
@@ -19,23 +19,23 @@ func SyncClusterRole(ctx context.Context, clusterRole *rbacv1.ClusterRole, clien
 		log.Info("Creating a new ClusterRole", "Name", clusterRole.Name)
 		err = client.Create(ctx, clusterRole)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		// we need to return found object
 		err := client.Get(ctx, types.NamespacedName{Name: clusterRole.Name, Namespace: clusterRole.Namespace}, found)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	} else if err != nil {
 		log.Info("Unknown error.", "Error", err.Error())
-		return nil, err
+		return err
 	} else {
 		log.Info("Updating ClusterRole", "Name:", clusterRole.Name)
 		err = client.Update(ctx, clusterRole)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
-	return found, nil
+	return nil
 }
