e1d69430473e4bdce51769ebbfdc9e44d3da698b
Christian Coffield <christian_coffield@dell.com>
2022-04-05 16:34:46 -0400

    change v1alpha1 to v1alpha2 (#69)
    * change v1alpha1 to v2alpha2
    
    * disable checks due to missing api/v1alpha2 in repo
    
    * add v1aplha2 folder

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ import (
 	crzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/yaml"
 
-	csmv1 "github.com/dell/csm-operator/api/v1alpha1"
+	csmv1 "github.com/dell/csm-operator/api/v1alpha2"
 	"github.com/dell/csm-operator/controllers"
 	"github.com/dell/csm-operator/core"
 	k8sClient "github.com/dell/csm-operator/k8s"
--- pkg/drivers/commonconfig.go
+++ pkg/drivers/commonconfig.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"path/filepath"
 
-	csmv1 "github.com/dell/csm-operator/api/v1alpha1"
+	csmv1 "github.com/dell/csm-operator/api/v1alpha2"
 	"github.com/dell/csm-operator/pkg/logger"
 	utils "github.com/dell/csm-operator/pkg/utils"
 	corev1 "k8s.io/api/core/v1"
