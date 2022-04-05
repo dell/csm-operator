30f9b52557f50eb5c881ab7d0bed997e6a01c8d4
kk3 <karthik.k1@dell.com>
2022-04-05 20:01:49 +0530

    olm install enhancement

--- scripts/uninstall_olm.sh
+++ scripts/uninstall_olm.sh
@@ -2,20 +2,20 @@
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOTDIR="$(dirname "$SCRIPTDIR")"
 DEPLOYDIR="$ROOTDIR/deploy/olm"
+source "$SCRIPTDIR"/common.bash
+
 
 # Constants
 COMMUNITY_MANIFEST="operator_community.yaml"
 MANIFEST_FILE="$DEPLOYDIR/$COMMUNITY_MANIFEST"
 
 # Set the namespace
-NS="test-csm-operator-olm"
+NAMESPACE="test-csm-operator-olm"
 
 # Get CSV name
-CSV=`kubectl get csv -n $NS --no-headers -o custom-columns=":metadata.name" | grep dell-csm-operator 2>&1`
-
+CSV=`kubectl get csv -n $NAMESPACE --no-headers -o custom-columns=":metadata.name" | grep dell-csm-operator 2>&1`
+log separator
 echo "Deleting the Operator Deployment"
-echo
-echo "*****"
 kubectl delete -f $MANIFEST_FILE
-kubectl delete csv $CSV -n $NS
+kubectl delete csv $CSV -n $NAMESPACE
 echo
