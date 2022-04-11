61b5aa6f69e0e2edb5a9d480a5c0c61a81d2640a
Jooseppi Luna <jooseppi_luna@dell.com>
2022-04-11 12:23:18 -0400

    one test not passing unsure why
    rebasing off main

--- controllers/csm_controller.go
+++ controllers/csm_controller.go
@@ -16,6 +16,8 @@ import (
 	"fmt"
 	"sync/atomic"
 	"time"
+	"strings"
+	"strconv"
 
 	"github.com/dell/csm-operator/pkg/drivers"
 	"github.com/dell/csm-operator/pkg/modules"
@@ -92,9 +94,6 @@ const (
 var (
 	dMutex           sync.RWMutex
 	configVersionKey = fmt.Sprintf("%s/%s", MetadataPrefix, "CSIoperatorConfigVersion")
-
-	// PScaleSupportedDriverVersions is a list of supported Powerscale driver versions
-	PScaleSupportedDriverVersions = map[string]bool{"v2.2.0": true, "v2.3.0": true}
 
 	// StopWatch - watcher stop handle
 	StopWatch = make(chan struct{})
@@ -695,6 +694,31 @@ func (r *ContainerStorageModuleReconciler) removeDriver(ctx context.Context, ins
 	return nil
 }
 
+// minVersionCheck takes a driver name and a version of the form "vA.B.C" and checks it against the minimum version for the specified driver
+func minVersionCheck(driverName string, version string) bool {
+	fmt.Printf("[minVersionCheck] version: %+v\n", version)
+	// strip v off of version string
+	versionNoV := strings.TrimLeft(version, "v")
+	fmt.Printf("[minVersionCheck] versionBits: %+v\n", versionNoV)
+	// split by .
+	versionBits := strings.Split(versionNoV, ".")
+	fmt.Printf("[minVersionCheck] versionBits: %+v\n", versionBits)
+	majorVersion, _ := strconv.Atoi(versionBits[0])
+	minorVersion, _ := strconv.Atoi(versionBits[1])
+	// compare each part according to minimum driver version
+	if driverName == "powerscale" {
+		// min version: v2.2.0
+		if majorVersion >= 2 && minorVersion >= 2 {
+			//log.Infow("unsupported version", "version", version)
+			return true
+		}
+		return false
+	} else {
+		//log.Infow("unknown driver name", "driverName", driverName)
+		return false
+	}
+}
+
 // PreChecks - validate input values
 func (r *ContainerStorageModuleReconciler) PreChecks(ctx context.Context, cr *csmv1.ContainerStorageModule, operatorConfig utils.OperatorConfig) error {
 	// Check drivers
@@ -704,7 +728,7 @@ func (r *ContainerStorageModuleReconciler) PreChecks(ctx context.Context, cr *cs
 		if err != nil {
 			return fmt.Errorf("failed powerscale validation: %v", err)
 		}
-		if cr.Spec.Driver.ConfigVersion == "" || !(PScaleSupportedDriverVersions[cr.Spec.Driver.ConfigVersion]) {
+		if cr.Spec.Driver.ConfigVersion == "" || !minVersionCheck("powerscale", cr.Spec.Driver.ConfigVersion) {
 			return fmt.Errorf("driver version not specified in spec or driver version is not supported")
 		}
 
