0d96e381eed9b243dc269ec06e522cda48203b00
karthikk92 <92289639+karthikk92@users.noreply.github.com>
2022-04-12 15:17:01 +0530

    KRV-3610 : Enhancement of instal_olm scripts in CSM operator (#71)

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,6 +2,7 @@
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOTDIR="$(dirname "$SCRIPTDIR")"
 DEPLOYDIR="$ROOTDIR/deploy"
+source "$SCRIPTDIR"/common.bash
 
 # find the operator namespace from operator.yaml file
 NS_STRING=$(cat ${DEPLOYDIR}/operator.yaml | grep "namespace:" | head -1)
@@ -11,8 +12,9 @@ if [ -z "${NS_STRING}" ]; then
 fi
 # find the namespace from the filtered string
 NAMESPACE=$(echo $NS_STRING | cut -d ' ' -f2)
-
+log separator
 echo "Deleting the Operator Deployment"
 echo
+log separator
 kubectl delete -f $DEPLOYDIR/operator.yaml
 echo
