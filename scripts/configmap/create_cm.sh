8f3fcccac1e8663b23a27178d290d59828ce66e3
Jooseppi Luna <jooseppi_luna@dell.com>
2022-04-14 13:34:10 -0400

    download remote files

--- scripts/configmap/create_cm.sh
+++ scripts/configmap/create_cm.sh
@@ -1,12 +1,21 @@
-k delete cm powerscale-driver-v2.2.0,driver-sidecars,authorization-v2.2.0,authorization-common -n dell-csm-operator
-kubectl create cm powerscale-driver-v2.2.0 --from-file=powerscale/v2.2.0/ -n dell-csm-operator
+# Download config tgz -- you will be required to put your personal access token in here as long as the csm-operator-config repo is private
+wget --header 'Authorization: token <insert token>' https://raw.githubusercontent.com/dell/csm-operator-config/fix-module-common/powerscale/powerscale-v2.2.0/downloads/pscale-v220-cfg.tgz
+
+# untar the config files, this will produce a folder called csmconfig
+tar -xzvf pscale-v220-cfg.tgz
+
+# remove the unnecessary tar file
+rm -f pscale-v220-cfg.tgz
+
+kubectl delete cm powerscale-driver-v2.2.0 driver-sidecars authorization-module-v1.2.0 authorization-common -n dell-csm-operator
+kubectl create cm powerscale-driver-v2.2.0 --from-file=csmconfig/driver/ -n dell-csm-operator
 kubectl get cm powerscale-driver-v2.2.0 -n dell-csm-operator
 
-kubectl create cm driver-sidecars --from-file=sidecars/ -n dell-csm-operator
-k get cm driver-sidecars -n dell-csm-operator
+kubectl create cm driver-sidecars --from-file=csmconfig/sidecars/ -n dell-csm-operator
+kubectl get cm driver-sidecars -n dell-csm-operator
 
-kubectl create cm authorization-v1.2.0 --from-file=authorization/v1.2.0 -n dell-csm-operator
-k get cm authorization-v2.2.0 -n dell-csm-operator
+kubectl create cm authorization-v1.2.0 --from-file=csmconfig/authorization-module-v1.2.0/ -n dell-csm-operator
+kubectl get cm authorization-module-v1.2.0 -n dell-csm-operator
 
-kubectl create cm authorization-common --from-file=auth-common/ -n dell-csm-operator
-k get cm authorization-common -n dell-csm-operator
+kubectl create cm module-common-v2.2.0 --from-file=csmconfig/module-common-v2.2.0/ -n dell-csm-operator
+kubectl get cm module-common-v2.2.0 -n dell-csm-operator
