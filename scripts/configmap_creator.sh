c7dea4664ba0a72bbecef97e47ec4a46278e590e
Jooseppi Luna <jooseppi_luna@dell.com>
2022-04-28 14:33:59 -0400

    allow local tgz and make improvements

--- scripts/configmap_creator.sh
+++ scripts/configmap_creator.sh
@@ -7,7 +7,7 @@ DRIVER="powerscale"
 DRIVERVERSION="v2.2.0"
 # All below variables not recommended for user modification
 DELETE_EXIST_CFGMAP=0
-CFGFOLDER=""
+TGZFOLDER=""
 TEMPDIR="/tmp/csm-oper-cfg-map/"
 PROG="${0}"
 CFGMAP_NAMES_FILE="cm_names.txt"
@@ -29,8 +29,8 @@ function cleanup_created_cfgmaps() {
 function read_configmap_names() {
   # make sure there is no existing list
   rm -rf $CFGMAP_NAMES_FILE
-  log section "Read configmap names from file $CFGFOLDER$DRIVER-$DRIVERVERSION.yaml"
-  cat $CFGFOLDER$DRIVER-$DRIVERVERSION.yaml | while read line
+  log section "Read configmap names from file $DRIVER-$DRIVERVERSION.yaml"
+  cat $DRIVER-$DRIVERVERSION.yaml | while read line
   do
     echo $line | grep listOfConfigMapNames
       if [ "$?" == "0" ]; then
@@ -60,7 +60,7 @@ function usage() {
   echo "Options:"
   echo "  Optional"
   echo "  --force-configmap-delete                 Force deletion of existing configmaps"
-  echo "  --use-local-config=<config-folder>       Use local configmaps in given folder"
+  echo "  --use-local-tgz=<tgz-file>               Use local tgz in given folder"
   echo "  -h                                       Help"
   echo
 
@@ -74,12 +74,12 @@ while getopts ":h-:" optchar; do
     force-configmap-delete)
       DELETE_EXIST_CFGMAP=1
       ;;
-    use-local-config)
-      CFGFOLDER="${!OPTIND}"
+    use-local-tgz)
+      TGZFOLDER="${!OPTIND}"
       OPTIND=$((OPTIND + 1))
       ;;
-    use-local-config=*)
-      CFGFOLDER=${OPTARG#*=}
+    use-local-tgz=*)
+      TGZFOLDER=${OPTARG#*=}
       ;;
     *)
       echo "Unknown option -${OPTARG}"
@@ -106,20 +106,18 @@ kubectl --help >&/dev/null || {
   exit 1
 }
 
-# if we are using local files, get the absolute path
-if [ $CFGFOLDER != "" ]; then
-  cd $CFGFOLDER
-  if [ "$?" != "0" ]; then
-    log error "the provided config files folder, $CFGFOLDER, was not found"
+# if we are using a local tgz, get the absolute path
+if [ "$TGZFOLDER" != "" ]; then
+  if [ -f $TGZFOLDER ]; then
+    # Make tgz name absolute path. If tgz name already is absolute path, this will change nothing
+    TGZFOLDERABS=`readlink -f $TGZFOLDER`
+    log step "using tar file located at $TGZFOLDERABS"
+  else
+    log error "the provided tgz, $TGZFOLDER, was not found"
     cleanup
     log Failed
     exit 4
   fi
-  CFGFOLDER=`pwd`
-  # Add trailing slash
-  CFGFOLDER="$CFGFOLDER/"
-  log step "using config files located at $CFGFOLDER"
-  cd -
 fi
 
 # Make temporary directory, move into it
@@ -127,8 +125,9 @@ mkdir $TEMPDIR
 cd $TEMPDIR
 
 
-if [ $CFGFOLDER == "" ]; then
+if [ "$TGZFOLDER" == "" ]; then
   log section "Downloading configmap yamls"
+
   # Download config tgz -- you will be required to put your personal access token in here as long as the csm-operator-config repo is private
   read -s -p 'Please enter GitHub token: ' github_token
   wget --header "Authorization: token ${github_token}" https://raw.githubusercontent.com/dell/csm-operator-config/main/$DRIVER/$DRIVER-$DRIVERVERSION/downloads/$DRIVER-$DRIVERVERSION.tgz
@@ -141,8 +140,10 @@ if [ $CFGFOLDER == "" ]; then
   fi
 
   log section "Untar config files"
-  # untar the config files
   tar -xzvf $DRIVER-$DRIVERVERSION.tgz
+else
+  log section "Untar config files"
+  tar -xzvf $TGZFOLDERABS
 fi
 
 # read configmap names from yaml file
@@ -168,12 +169,12 @@ do
   fi
 
   # create new configmap
-  kubectl create cm $cfgmap --from-file=$CFGFOLDER$cfgmap -n $NS
+  kubectl create cm $cfgmap --from-file=$cfgmap -n $NS
 
   # check the configmap exists
   kubectl get configmap -n $NS | grep -q $cfgmap
   if [ "$?" == "0" ]; then
-    cfgmaps_created="$cfgmaps_created $cfgmap"
+    cfgmaps_created="$cfgmaps_created$cfgmap "
     log step "configmap $cfgmap successfully created"
   else
     log error "configmap $cfgmap not successfully created"
@@ -186,7 +187,14 @@ do
 done <<<`cat $CFGMAP_NAMES_FILE`
 log section "Finishing configmap creation"
 
-log step "Following configmaps created: $cfgmaps_created"
+if [ "$cfgmaps_created" == "" ]; then
+  log step "No new configmaps created"
+else
+  # remove trailing space then insert commas
+  cfgmaps_created=${cfgmaps_created%?}
+  log step "Following configmaps created: ${cfgmaps_created//[ ]/, }"
+fi
+
 log Passed
 cleanup
 
