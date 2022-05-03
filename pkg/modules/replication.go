4d8d21effea7e3ac5981f3fa8bf93e89c57216a0
Christian Coffield <christian_coffield@dell.com>
2022-05-03 09:04:36 -0400

    set package to v1alpha1 for crd (#82)
    * set package to v1alpha1 for crd
    run unit tests ok
    make image ok
    deploy driver ok
    
    * remove local image

--- pkg/modules/replication.go
+++ pkg/modules/replication.go
@@ -7,7 +7,7 @@ import (
 	"os/exec"
 	"strings"
 
-	csmv1 "github.com/dell/csm-operator/api/v1alpha2"
+	csmv1 "github.com/dell/csm-operator/api/v1alpha1"
 
 	"github.com/dell/csm-operator/pkg/drivers"
 	"github.com/dell/csm-operator/pkg/logger"
