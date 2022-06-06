ad69a185a8b1d5c32f721ba2a508571e587c36e3
rensyct <80810999+rensyct@users.noreply.github.com>
2022-06-06 19:29:15 +0530

    Update UBI image, manifests and Readme (#91)

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ const (
 	// K8sMinimumSupportedVersion is the minimum supported version for k8s
 	K8sMinimumSupportedVersion = "1.21"
 	// K8sMaximumSupportedVersion is the maximum supported version for k8s
-	K8sMaximumSupportedVersion = "1.23"
+	K8sMaximumSupportedVersion = "1.24"
 )
 
 var (
