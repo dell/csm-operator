3ee1e537dce1b48bdc21b2ebf61102c23ff7b702
rensyct <80810999+rensyct@users.noreply.github.com>
2022-06-22 10:31:53 +0530

    Update module version in manifests (#93)

--- pkg/modules/replication.go
+++ pkg/modules/replication.go
@@ -306,7 +306,7 @@ func getReplicaController(op utils.OperatorConfig, cr csmv1.ContainerStorageModu
 	replicaCount := "1"
 	retryMin := "1s"
 	retryMax := "5m"
-	replicaImage := "dellemc/dell-replication-controller:v1.2.0"
+	replicaImage := "dellemc/dell-replication-controller:v1.3.0"
 
 	for _, component := range replica.Components {
 		if component.Name == utils.ReplicationControllerManager {
