a19b2d4e822bebf2d47e16813c7eb4967ca63bc8
Jooseppi Luna <jooseppi_luna@dell.com>
2022-09-08 15:08:35 -0400

    e2e tests working and improved

---
+++ tests/e2e/env-e2e-test.sh
@@ -0,0 +1,14 @@
+# [Optional] ginko options for custom runs
+export GINKGO_OPTS="-v"
+
+# [Optional] Path to .kube configuration if it is not in the default location
+# export KUBECONFIG=""
+
+# Must supply path to values file
+export VALUES_FILE="testfiles/values.yaml"
+
+# Must supply path to cert-csi binary
+export CERT_CSI="/root/cert-csi"
+
+# [Optional] namespace of operator if you deployed it to a namespace diffrent form the one below.
+# export OPERATOR_NAMESPACE="dell-csm-operator"
