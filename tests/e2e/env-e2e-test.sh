2a72624a4eae438f857004cf358eac1e3aa3787b
Jooseppi Luna <jooseppi_luna@dell.com>
2022-09-15 09:10:54 -0400

    add modules field in test spec

--- tests/e2e/env-e2e-test.sh
+++ tests/e2e/env-e2e-test.sh
@@ -10,5 +10,9 @@ export VALUES_FILE="testfiles/values.yaml"
 # Must supply path to cert-csi binary
 export CERT_CSI="/root/cert-csi"
 
+# [Optional] set any modules you want to test to true
+# export AUTHORIZATION=true
+# export REPLICATION=true
+
 # [Optional] namespace of operator if you deployed it to a namespace diffrent form the one below.
 # export OPERATOR_NAMESPACE="dell-csm-operator"
