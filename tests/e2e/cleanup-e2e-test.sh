a6666932219d9d1d7436a04b4b6e61548dde2566
Jooseppi Luna <jooseppi_luna@dell.com>
2022-10-14 16:04:47 -0400

    E2e ginkgo 1 16 5 (#104)
    * e2e tests working and improved
    
    * Update README.md
    
    * add modules field in test spec
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * fixed gofmt added cleanup
    
    * Update README.md
    
    * Update allowedlist.yaml
    
    * Update allowedlist.yaml
    
    * Update values.yaml
    
    * e2e tests working and improved
    
    * add modules field in test spec
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * fixed gofmt added cleanup
    
    * Update README.md
    
    * Update allowedlist.yaml
    
    * Update allowedlist.yaml
    
    * Bump up version to 1.0.0 (#108)
    
    * Removed links from TOC (#107)
    
    * Updated the go version to 1.19 from 1.18 (#109)
    
    * updated from v1alpha1 to v1 (#110)
    
    * Update values.yaml
    
    * observability
    
    Co-authored-by: rensyct <80810999+rensyct@users.noreply.github.com>
    Co-authored-by: gallacher <35462391+gallacher@users.noreply.github.com>
    Co-authored-by: karthikk92 <92289639+karthikk92@users.noreply.github.com>

---
+++ tests/e2e/cleanup-e2e-test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+PROG="${0}"
+
+# usage will print command execution help and then exit
+function usage() {
+  echo
+  echo "Help for $PROG"
+  echo
+  echo "Usage: $PROG options..."
+  echo "Options:"
+  echo "  Optional"
+  echo "  --keep-logs                              Do not delete logfiles in cleanup"
+  echo "  -h                                       Help"
+  echo
+
+  exit 0
+}
+
+while getopts ":h-:" optchar; do
+  case "${optchar}" in
+  -)
+    case "${OPTARG}" in
+    keep-logs)
+      KEEPLOGS=1
+      ;;
+    *)
+      decho "Unknown option --${OPTARG}"
+      decho "For help, run $PROG -h"
+      exit 1
+      ;;
+    esac
+    ;;
+  h)
+    usage
+    ;;
+  *)
+    decho "Unknown option -${OPTARG}"
+    decho "For help, run $PROG -h"
+    exit 1
+    ;;
+  esac
+done
+
+rm -f isilon.db
+
+if [ -z "$KEEPLOGS" ]; then
+    rm -f error.log fatal.log info.log report.path
+fi
+
+exit 0
