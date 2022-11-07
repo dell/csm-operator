888f57931eb047fa722589ab8b8134d31dbe74e6
Jooseppi Luna <jooseppi_luna@dell.com>
2022-11-07 12:16:15 -0500

    added pflex tests bumped coverage to 95.9

--- pkg/drivers/common_test.go
+++ pkg/drivers/common_test.go
@@ -25,6 +25,10 @@ var (
 		ConfigDirectory: "../../tests/config",
 	}
 
+	pflexCSMName              = "pflex-csm"
+	pflexCredsName            = pflexCSMName + "-config"
+	pFlexNS                   = "pflex-test"
+
 	trueBool  bool = true
 	falseBool bool = false
 )
@@ -92,3 +96,40 @@ func csmWithTolerations(driver csmv1.DriverType, version string) csmv1.Container
 
 	return res
 }
+
+// JJL make a functino that uses the standard name and calls this function
+// makes a pflex csm object
+func csmForPowerFlex(customCSMName string) csmv1.ContainerStorageModule {
+	res := shared.MakeCSM(customCSMName, pFlexNS, shared.PFlexConfigVersion)
+
+	// Add sdc initcontainer
+	res.Spec.Driver.InitContainers = []csmv1.ContainerTemplate{csmv1.ContainerTemplate{
+		Name:            "sdc",
+		Enabled:         &trueBool,
+		Image:           "image",
+		ImagePullPolicy: "IfNotPresent",
+		Args:            []string{},
+		Envs:            []corev1.EnvVar{corev1.EnvVar{Name: "MDM"}},
+		Tolerations:     []corev1.Toleration{},
+	}}
+
+	// Add sdc-monitor Sidecar
+	res.Spec.Driver.SideCars = []csmv1.ContainerTemplate{csmv1.ContainerTemplate{
+		Name:            "sdc-monitor",
+		Enabled:         &falseBool,
+		Image:           "image",
+		ImagePullPolicy: "IfNotPresent",
+		Args:            []string{},
+		Envs:            []corev1.EnvVar{corev1.EnvVar{Name: "MDM"}},
+		Tolerations:     []corev1.Toleration{},
+	}}
+
+	//res.Spec.Driver.CSIDriverSpec.FSGroupPolicy == "ReadWriteOnceWithFSType"
+
+	// Add pflex driver version
+	res.Spec.Driver.ConfigVersion = shared.PFlexConfigVersion
+	res.Spec.Driver.CSIDriverType = csmv1.PowerFlex
+
+	return res
+}
+
--- pkg/drivers/commonconfig_test.go
+++ pkg/drivers/commonconfig_test.go
@@ -26,6 +26,7 @@ import (
 
 var (
 	csm = csmWithTolerations(csmv1.PowerScaleName, "v2.2.0")
+	pFlexCSM = csmForPowerFlex(pflexCSMName)
 
 	fakeDriver csmv1.DriverType = "fakeDriver"
 	badDriver  csmv1.DriverType = "badDriver"
@@ -42,7 +43,8 @@ var (
 		// expected error
 		expectedErr string
 	}{
-		{"happy path", csm, csmv1.PowerScaleName, "node.yaml", ""},
+		{"pscale happy path", csm, csmv1.PowerScaleName, "node.yaml", ""},
+		{"pflex happy path", pFlexCSM, csmv1.PowerFlex, "node.yaml", ""},
 		{"file does not exist", csm, fakeDriver, "NonExist.yaml", "no such file or directory"},
 		{"config file is invalid", csm, badDriver, "bad.yaml", "unmarshal"},
 	}
