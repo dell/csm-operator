bf9514a405c03014c130f0bb2ca846d3f6c06b86
P-Cao <105041254+P-Cao@users.noreply.github.com>
2022-11-07 15:17:01 +0800

    Support PowerFlex Observability Module (#122)
    * Support topology component
    * Support powerflex metrics component for both 1.3.0 & 1.2.0
    * Support Otel collector
    * Add related UT
    * Add related e2e

--- tests/e2e/steps/step_common.go
+++ tests/e2e/steps/step_common.go
@@ -31,6 +31,8 @@ import (
 var defaultObservabilityDeploymentName = map[csmv1.DriverType]string{
 	csmv1.PowerScaleName: "karavi-metrics-powerscale",
 	csmv1.PowerScale:     "karavi-metrics-powerscale",
+	csmv1.PowerFlexName:  "karavi-metrics-powerflex",
+	csmv1.PowerFlex:      "karavi-metrics-powerflex",
 }
 
 // CustomTest -
