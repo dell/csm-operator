67924148257c691446410a535b81835ca72256df
karthikk92 <92289639+karthikk92@users.noreply.github.com>
2023-02-14 16:24:16 +0530

    Ensure that the fields mentioned in samples are configurable. (#162)

--- pkg/drivers/powerstore.go
+++ pkg/drivers/powerstore.go
@@ -15,10 +15,15 @@ package drivers
 import (
 	"context"
 	"fmt"
+	"os"
+	"strings"
+
 	csmv1 "github.com/dell/csm-operator/api/v1"
 	"github.com/dell/csm-operator/pkg/logger"
 	"github.com/dell/csm-operator/pkg/utils"
-	"os"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -28,6 +33,21 @@ const (
 
 	// PowerStoreConfigParamsVolumeMount -
 	PowerStoreConfigParamsVolumeMount = "csi-powerstore-config-params"
+
+	// CsiPowerstoreNodeNamePrefix - Node Name Prefix
+	CsiPowerstoreNodeNamePrefix = "<X_CSI_POWERSTORE_NODE_NAME_PREFIX>"
+
+	// CsiFcPortFilterFilePath - Fc Port Filter File Path
+	CsiFcPortFilterFilePath = "<X_CSI_FC_PORTS_FILTER_FILE_PATH>"
+
+	// CsiNfsAcls - variable setting the permissions on NFS mount directory
+	CsiNfsAcls = "<X_CSI_NFS_ACLS>"
+
+	// CsiHealthMonitorEnabled - health monitor flag
+	CsiHealthMonitorEnabled = "<X_CSI_HEALTH_MONITOR_ENABLED>"
+
+	// CsiPowerstoreEnableChap -  CHAP flag
+	CsiPowerstoreEnableChap = "<X_CSI_POWERSTORE_ENABLE_CHAP>"
 )
 
 // PrecheckPowerStore do input validation
@@ -44,6 +64,64 @@ func PrecheckPowerStore(ctx context.Context, cr *csmv1.ContainerStorageModule, o
 	}
 	secrets := []string{config}
 
+	for _, name := range secrets {
+		found := &corev1.Secret{}
+		err := ct.Get(ctx, types.NamespacedName{Name: name, Namespace: cr.GetNamespace()}, found)
+		if err != nil {
+			log.Error(err, "Failed query for secret ", name)
+			if errors.IsNotFound(err) {
+				return fmt.Errorf("failed to find secret %s", name)
+			}
+		}
+	}
+
 	log.Debugw("preCheck", "secrets", len(secrets))
 	return nil
+}
+
+// ModifyPowerstoreCR -
+func ModifyPowerstoreCR(yamlString string, cr csmv1.ContainerStorageModule, fileType string) string {
+	// Parameters to initialise CR values
+	nodePrefix := ""
+	fcPortFilter := ""
+	nfsAcls := ""
+	healthMonitorController := ""
+	chap := ""
+	healthMonitorNode := ""
+
+	switch fileType {
+	case "Node":
+		for _, env := range cr.Spec.Driver.Common.Envs {
+			if env.Name == "X_CSI_POWERSTORE_NODE_NAME_PREFIX" {
+				nodePrefix = env.Value
+			}
+			if env.Name == "X_CSI_FC_PORTS_FILTER_FILE_PATH" {
+				fcPortFilter = env.Value
+			}
+		}
+		for _, env := range cr.Spec.Driver.Node.Envs {
+			if env.Name == "X_CSI_POWERSTORE_ENABLE_CHAP" {
+				chap = env.Value
+			}
+			if env.Name == "X_CSI_HEALTH_MONITOR_ENABLED" {
+				healthMonitorNode = env.Value
+			}
+		}
+		yamlString = strings.ReplaceAll(yamlString, CsiPowerstoreNodeNamePrefix, nodePrefix)
+		yamlString = strings.ReplaceAll(yamlString, CsiFcPortFilterFilePath, fcPortFilter)
+		yamlString = strings.ReplaceAll(yamlString, CsiPowerstoreEnableChap, chap)
+		yamlString = strings.ReplaceAll(yamlString, CsiHealthMonitorEnabled, healthMonitorNode)
+	case "Controller":
+		for _, env := range cr.Spec.Driver.Controller.Envs {
+			if env.Name == "X_CSI_NFS_ACLS" {
+				nfsAcls = env.Value
+			}
+			if env.Name == "X_CSI_HEALTH_MONITOR_ENABLED" {
+				healthMonitorController = env.Value
+			}
+		}
+		yamlString = strings.ReplaceAll(yamlString, CsiNfsAcls, nfsAcls)
+		yamlString = strings.ReplaceAll(yamlString, CsiHealthMonitorEnabled, healthMonitorController)
+	}
+	return yamlString
 }
