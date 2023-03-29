e7c2d186f9f9aef37533a64c083e4334ca8d384d
Kumar Yadav <Adarsh.KumarYadav@dell.com>
2023-03-29 16:39:14 +0530

    UT first step

--- pkg/modules/resiliency_test.go
+++ pkg/modules/resiliency_test.go
@@ -13,7 +13,60 @@
 // limitations under the License.
 package modules
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	csmv1 "github.com/dell/csm-operator/api/v1"
+	drivers "github.com/dell/csm-operator/pkg/drivers"
+	utils "github.com/dell/csm-operator/pkg/utils"
+	"github.com/stretchr/testify/assert"
+	rbacv1 "k8s.io/api/rbac/v1"
+)
+
+func TestResiliencyInjectClusterRole(t *testing.T) {
+	ctx := context.Background()
+
+	tests := map[string]func(t *testing.T) (bool, rbacv1.ClusterRole, utils.OperatorConfig, csmv1.ContainerStorageModule){
+		"success - greenfield injection": func(*testing.T) (bool, rbacv1.ClusterRole, utils.OperatorConfig, csmv1.ContainerStorageModule) {
+			customResource, err := getCustomResource("./testdata/cr_powerstore_resiliency.yaml")
+			if err != nil {
+				panic(err)
+			}
+			controllerYAML, err := drivers.GetController(ctx, customResource, operatorConfig, csmv1.PowerStore)
+			if err != nil {
+				panic(err)
+			}
+			return true, controllerYAML.Rbac.ClusterRole, operatorConfig, customResource
+		},
+		"fail - bad config path": func(*testing.T) (bool, rbacv1.ClusterRole, utils.OperatorConfig, csmv1.ContainerStorageModule) {
+			tmpOperatorConfig := operatorConfig
+			tmpOperatorConfig.ConfigDirectory = "bad/path"
+
+			customResource, err := getCustomResource("./testdata/cr_powerscale_replica.yaml")
+			if err != nil {
+				panic(err)
+			}
+			controllerYAML, err := drivers.GetController(ctx, customResource, operatorConfig, csmv1.PowerStore)
+			if err != nil {
+				panic(err)
+			}
+			return false, controllerYAML.Rbac.ClusterRole, tmpOperatorConfig, customResource
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			success, clusterRole, opConfig, cr := tc(t)
+			newClusterRole, err := ResiliencyInjectClusterRole(clusterRole, cr, opConfig, "node")
+			if success {
+				assert.NoError(t, err)
+				assert.NoError(t, CheckClusterRoleReplica(newClusterRole.Rules))
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
 
 func TestResiliencyPreCheck(t *testing.T) {
 
