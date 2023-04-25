fb97ead059bc56a00572a6f57afcaea8e46b7805
Jooseppi Luna <jooseppi_luna@dell.com>
2023-04-25 15:04:27 -0400

    application-mobility e2e testing framework (#208)
    * got e2e test working
    
    * Update steps_def.go
    
    * added replicacount 2 e2e test for reconcileAppMobility
    
    ---------
    
    Co-authored-by: Abrar Basha <abrar.basha@dell.com>

--- tests/e2e/go.mod
+++ tests/e2e/go.mod
@@ -26,6 +26,7 @@ require (
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.2.2 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
@@ -66,12 +67,13 @@ require (
 	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
-	golang.org/x/net v0.1.1-0.20221104162952-702349b0e862 // indirect
+	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
-	golang.org/x/sys v0.2.0 // indirect
-	golang.org/x/term v0.1.0 // indirect
-	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
+	golang.org/x/tools v0.8.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20210831024726-fe130286e0e2 // indirect
 	google.golang.org/grpc v1.40.0 // indirect
---
+++ tests/e2e/verify-app-mobility.sh
@@ -0,0 +1,81 @@
+BACKUP_NAME_EXT=$(date +%s)
+BACKUP_NAME=b$BACKUP_NAME_EXT
+RESTORE_NAME=r$BACKUP_NAME_EXT
+VOL_NS=helmtest-vxflexos
+RES_NS=res-vxflexos
+
+
+# no need to check for pod success since e2e already does that
+
+
+# attempt backup, check if successful
+dellctl backup create $BACKUP_NAME --include-namespaces $VOL_NS
+
+
+# check return code from backup command
+RET=$?
+if [ "${RET}" != "0" ]; then
+  echo "backup failed with return code $RET"
+  exit $RET
+fi
+
+
+# give the backup resource 5 minutes to succeed
+BACKUP_WAIT_TIME=$((SECONDS+30))
+sleep 5
+while [ $SECONDS -lt $BACKUP_WAIT_TIME ]; do
+  NUM_GOOD_BACKUPS=$(dellctl backup get $BACKUP_NAME | grep "Completed" | wc -l)
+  if [ "${NUM_GOOD_BACKUPS}" == "1" ]; then
+    echo "backup successful"
+    break
+  fi
+  echo "waiting for backup to complete"
+  sleep 5
+done
+
+
+if [ "${NUM_GOOD_BACKUPS}" != "1" ]; then
+  echo -e "backup not completed -- backup current status:"
+  dellctl backup get $BACKUP_NAME
+  exit 1
+fi
+
+
+# attempt restore, check if successful
+dellctl restore create $RESTORE_NAME --from-backup $BACKUP_NAME --namespace-mappings $VOL_NS:$RES_NS
+RET=$?
+if [ "${RET}" != "0" ]; then
+  echo "restore failed with return code $RET"
+  exit $RET
+fi
+
+
+# give the backup resource 5 minutes to succeed
+RESTORE_WAIT_TIME=$((SECONDS+30))
+sleep 5
+while [ $SECONDS -lt $RESTORE_WAIT_TIME ]; do
+  NUM_GOOD_RESTORE=$(dellctl restore get $RESTORE_NAME | grep "Completed" | wc -l)
+  if [ "${NUM_GOOD_RESTORE}" == "1" ]; then
+    echo "restore successful"
+    break
+  fi
+  echo "waiting for restore to complete"
+  sleep 5
+done
+
+
+if [ "${NUM_GOOD_RESTORE}" != "1" ]; then
+  echo -e "backup not completed -- backup current status:"
+  dellctl backup get $RESTORE_NAME
+  exit 1
+fi
+
+
+# success -- delete test backup
+kubectl delete backups.mobility.storage.dell.com/$BACKUP_NAME
+kubectl patch backups.mobility.storage.dell.com/$BACKUP_NAME \
+    --type json \
+    --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
+
+
+exit 0
