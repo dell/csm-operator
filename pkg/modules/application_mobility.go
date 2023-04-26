05e3ff5a8f5c4ece4ebd0987517061ce95d9bcfa
Jooseppi Luna <jooseppi_luna@dell.com>
2023-04-26 14:54:04 -0400

    commented cert-manager code back out

--- pkg/modules/application_mobility.go
+++ pkg/modules/application_mobility.go
@@ -263,7 +263,7 @@ func ApplicationMobilityPrecheck(ctx context.Context, op utils.OperatorConfig, a
 }
 
 // AppMobilityCertManager - Install/Delete cert-manager
-func AppMobilityCertManager(ctx context.Context, isDeleting bool, op utils.OperatorConfig, cr csmv1.ContainerStorageModule, ctrlClient crclient.Client) error {
+/*func AppMobilityCertManager(ctx context.Context, isDeleting bool, op utils.OperatorConfig, cr csmv1.ContainerStorageModule, ctrlClient crclient.Client) error {
 	YamlString, err := getAppMobCertManager(op, cr)
 	if err != nil {
 		return err
@@ -305,7 +305,7 @@ func getAppMobCertManager(op utils.OperatorConfig, cr csmv1.ContainerStorageModu
 }
 
 // AppMobilityVelero - Install/Delete velero
-/*func AppMobilityVelero(ctx context.Context, isDeleting bool, op utils.OperatorConfig, cr csmv1.ContainerStorageModule, ctrlClient crclient.Client) error {
+func AppMobilityVelero(ctx context.Context, isDeleting bool, op utils.OperatorConfig, cr csmv1.ContainerStorageModule, ctrlClient crclient.Client) error {
 	YamlString, err := getVelero(op, cr)
 	if err != nil {
 		return err
