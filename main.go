d68e26574169966d5aff64c57a67de9ebd29777c
Christian Coffield <christian_coffield@dell.com>
2023-05-04 00:26:21 +0530

    powerscale 2.7 changes (#216)
    * powerscale 2.7 changes
    rebase
    
    * Removed 2.4.0 files
    
    * Enable modules for 2.7.0
    
    * UT-fix

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//  Copyright © 2021 - 2022 Dell Inc. or its subsidiaries. All Rights Reserved.
+//  Copyright © 2021 - 2023 Dell Inc. or its subsidiaries. All Rights Reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -58,7 +58,7 @@ const (
 	// K8sMinimumSupportedVersion is the minimum supported version for k8s
 	K8sMinimumSupportedVersion = "1.21"
 	// K8sMaximumSupportedVersion is the maximum supported version for k8s
-	K8sMaximumSupportedVersion = "1.26"
+	K8sMaximumSupportedVersion = "1.27"
 )
 
 var (
