8e95ee5a397d6acd9e4ab256f398e94270a2440c
karthikk92 <92289639+karthikk92@users.noreply.github.com>
2023-07-31 15:28:52 +0530

    Update unity driver version to 2.8.0 (#300)

--- tests/shared/common.go
+++ tests/shared/common.go
@@ -34,8 +34,8 @@ const (
 	OldConfigVersion         string = "v2.2.0"
 	BadConfigVersion         string = "v0"
 	PStoreConfigVersion      string = "v2.8.0"
-	UnityConfigVersion       string = "v2.6.0"
 	PScaleConfigVersion      string = "v2.8.0"
+	UnityConfigVersion       string = "v2.8.0"
 	PmaxConfigVersion        string = "v2.8.0"
 )
 
