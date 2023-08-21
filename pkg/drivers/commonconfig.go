d04393484b4068a1d88dd866083088c6a5a91feb
Rajendra Indukuri <82365588+rajendraindukuri@users.noreply.github.com>
2023-08-21 12:36:50 +0530

    Updated common config file as part of SCT feature (#324)
    * Updated missing common config file

--- pkg/drivers/commonconfig.go
+++ pkg/drivers/commonconfig.go
@@ -398,6 +398,8 @@ func GetCSIDriver(ctx context.Context, cr csmv1.ContainerStorageModule, operator
 		YamlString = ModifyPowerScaleCR(YamlString, cr, "CSIDriverSpec")
 	case "powermax":
 		YamlString = ModifyPowermaxCR(YamlString, cr, "CSIDriverSpec")
+	case "unity":
+		YamlString = ModifyUnityCR(YamlString, cr, "CSIDriverSpec")
 	}
 	err = yaml.Unmarshal([]byte(YamlString), &csidriver)
 	if err != nil {
