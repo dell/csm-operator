9f056869b2750001058a37b12eb0f01801b97e41
abhi16394 <aabhi66@gmail.com>
2023-10-13 14:58:10 -0400

    Fix lint errors

--- controllers/csm_controller.go
+++ controllers/csm_controller.go
@@ -101,7 +101,7 @@ var (
 	configVersionKey                = fmt.Sprintf("%s/%s", MetadataPrefix, "CSMOperatorConfigVersion")
 	previouslyAppliedCustomResource = fmt.Sprintf("%s/%s", MetadataPrefix, "previously-applied-configuration")
         // CSMVersionKey - CSM Module version
-	CSMVersionKey                   = fmt.Sprintf("%s/%s", MetadataPrefix, "CSMVersion")
+	CSMVersionKey = fmt.Sprintf("%s/%s", MetadataPrefix, "CSMVersion")
 
 	// StopWatch - watcher stop handle
 	StopWatch = make(chan struct{})
