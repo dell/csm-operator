5ad19a5f7646274cb854694d8caa2a7f1070ad5c
Don Khan <don.khan@dell.com>
2023-11-06 10:58:45 -0500

    Add role kubebuilder config.

--- controllers/acc_controller.go
+++ controllers/acc_controller.go
@@ -122,6 +122,7 @@ var (
 //+kubebuilder:rbac:groups=storage.dell.com,resources=apexconnectivityclients/finalizers,verbs=update
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+//+kubebuilder:rbac:groups=mobility.storage.dell.com,resources=backups,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
