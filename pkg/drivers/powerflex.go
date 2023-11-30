93c21bc0bdcf4e3644ec33f31f20ffe8f5aaf232
Surya Gupta <Surya.Gupta1@dell.com>
2023-11-30 13:19:30 -0500

    lint fix

--- pkg/drivers/powerflex.go
+++ pkg/drivers/powerflex.go
@@ -246,7 +246,7 @@ func ModifyPowerflexCR(yamlString string, cr csmv1.ContainerStorageModule, fileT
 			}
 		}
 		yamlString = strings.ReplaceAll(yamlString, CsiHealthMonitorEnabled, healthMonitorController)
-    yamlString = strings.ReplaceAll(yamlString, CsiPowerflexExternalAccess, powerflexExternalAccess)
+		yamlString = strings.ReplaceAll(yamlString, CsiPowerflexExternalAccess, powerflexExternalAccess)
 
 	case "Node":
 		for _, env := range cr.Spec.Driver.Node.Envs {
