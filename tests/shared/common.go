d01383ba688b30d000ee657eff423d048a0e375e
Kshitija Kakde <Kshitija_Kakde@Dell.com>
2024-01-23 18:02:56 +0530

    updated version to 2.9.1 for unity, powerflex, powerscale and powermax

--- tests/shared/common.go
+++ tests/shared/common.go
@@ -30,7 +30,7 @@ const (
 	PFlexConfigVersion       string = "v2.9.1"
 	ConfigVersion            string = "v2.7.0"
 	UpgradeConfigVersion     string = "v2.8.0"
-	JumpUpgradeConfigVersion string = "v2.9.0"
+	JumpUpgradeConfigVersion string = "v2.9.1"
 	OldConfigVersion         string = "v2.2.0"
 	BadConfigVersion         string = "v0"
 	PStoreConfigVersion      string = "v2.9.0"
