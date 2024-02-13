c2f5f41ddbf2d153c9fbdf289fcb106804c099cd
Jooseppi Luna <jooseppi_luna@dell.com>
2024-02-13 17:36:21 -0500

    removing commented code

--- pkg/utils/status.go
+++ pkg/utils/status.go
@@ -1007,7 +1007,4 @@ func authProxyStatusCheck(ctx context.Context, instance *csmv1.ContainerStorageM
 	log.Info("auth proxy deployment successful")
 
 	return true, nil
-
-	//return proxyServerRunning && redisCommanderRunning && redisPrimaryRunning && roleServiceRunning && storageServiceRunning && tenantServiceRunning &&
-		//(!certEnabled || (certManagerRunning && certManagerCainInjectorRunning && certManagerWebhookRunning)) && (!nginxEnabled || nginxRunning), nil
 }
