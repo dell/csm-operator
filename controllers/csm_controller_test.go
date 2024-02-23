edecb7a70abdd673e3eef23094cc332e05e0e466
Jooseppi Luna <jooseppi_luna@dell.com>
2024-02-23 12:12:21 -0500

    Automate certificate creation for observability (#494)
    * add cert creation for observability
    
    * Create selfsigned-cert.yaml
    
    * add automatic self-signed cert creation
    
    * add imports
    
    * add custom cert for observability
    
    * add certificate and private key types
    
    * add certificate and privatekey to obs sample file
    
    * move IssuerCertService into common
    
    * issuer cert service
    
    * OtelCollector constant
    
    * add custom cert logic
    
    * Update types.go
    
    * Update types.go
    
    * base64-encode certificate and private key
    
    * add print to test to figure things out
    
    * fix compilation errors
    
    * compilation errors
    
    * println to printf
    
    * add logging to understand function
    
    * Update csm_controller.go
    
    * handle each component cert separately
    
    * I think i fixed something
    
    * Create otel-collector-custom-cert.yaml
    
    * Update custom-cert
    
    * make custom cert generic
    
    * make component generic
    
    * move to obs directory and edit obs component placeholder name
    
    * move to obs folder and adjust placeholder names
    
    * update placeholder name
    
    * update placeholder name
    
    * Update observability.go
    
    * fix error check
    
    * remove comma
    
    * update map declaration
    
    * fix map declaration
    
    * compilation issues
    
    * compilation error
    
    * compilation errors
    
    * compilation errors + code cleanup
    
    * add error check
    
    * add retry logic in applyobject
    
    * add import
    
    * bruh
    
    * added unit test coverage to controllers
    
    * generate/manifests/install
    
    * added full unit test coverage of added code
    
    * get rid of misc unneeded changes
    
    * add cert and privatekey to obs
    
    * add cert and privatekey
    
    * add sample files
    
    * add new e2e test scenarios
    
    * add e2e test files
    
    * fix lint
    
    * fix linting
    
    * update samples based on review

--- controllers/csm_controller_test.go
+++ controllers/csm_controller_test.go
@@ -1396,6 +1396,7 @@ func (suite *CSMControllerTestSuite) TestReconcileObservabilityError() {
 	assert.NotNil(suite.T(), err)
 
 	for i := range csm.Spec.Modules[0].Components {
+		fmt.Printf("Component name: %s\n", csm.Spec.Modules[0].Components[i].Name)
 		csm.Spec.Modules[0].Components[i].Enabled = &[]bool{false}[0]
 		err = reconciler.reconcileObservability(ctx, false, badOperatorConfig, csm, nil, suite.fakeClient, suite.k8sClient)
 		if i < len(csm.Spec.Modules[0].Components)-1 {
@@ -1431,6 +1432,32 @@ func (suite *CSMControllerTestSuite) TestReconcileObservabilityErrorBadComponent
 
 	goodModules := csm.Spec.Modules[0].Components
 	csm.Spec.Modules[0].Components = append(badComponent, csm.Spec.Modules[0].Components...)
+
+	err := reconciler.reconcileObservability(ctx, false, operatorConfig, csm, nil, suite.fakeClient, suite.k8sClient)
+	assert.NotNil(suite.T(), err)
+
+	csm.Spec.Modules[0].Components = goodModules
+}
+
+func (suite *CSMControllerTestSuite) TestReconcileObservabilityErrorBadCert() {
+	csm := shared.MakeCSM(csmName, suite.namespace, configVersion)
+	csm.Spec.Modules = getObservabilityModule()
+	reconciler := suite.createReconciler()
+
+	goodModules := csm.Spec.Modules[0].Components
+	for index, component := range csm.Spec.Modules[0].Components {
+		if component.Name == "topology" {
+			csm.Spec.Modules[0].Components[index].Certificate = "bad-cert"
+		}
+		if component.Name == "metrics-powerscale" {
+			csm.Spec.Modules[0].Components[index].Enabled = &[]bool{false}[0]
+		}
+		if component.Name == "metrics-powerflex" {
+			csm.Spec.Modules[0].Components[index].Enabled = &[]bool{false}[0]
+		}
+	}
+
+	fmt.Printf("[TestReconcileObservabilityErrorBadCert] module components: %+v\n", csm.Spec.Modules[0].Components)
 
 	err := reconciler.reconcileObservability(ctx, false, operatorConfig, csm, nil, suite.fakeClient, suite.k8sClient)
 	assert.NotNil(suite.T(), err)
