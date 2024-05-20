12b8e40b16613749ef40d3f7ee049ba806d16bf2
Jooseppi Luna <jooseppi_luna@dell.com>
2024-05-20 07:41:29 -0400

    Update array-info.sh

--- tests/e2e/array-info.sh
+++ tests/e2e/array-info.sh
@@ -17,41 +17,41 @@ export PROXY_HOST="csm-authorization.com"
 # The following are Authorization Proxy Server specific:
 # Must supply storage array details
 # Storage type examples - powerscale, powerflex, powermax
-# export STORAGE_TYPE="powerscale"
-# export END_POINT="1.1.1.1:8080"
-# export SYSTEM_ID="xxxxxx"
-# export STORAGE_USER="xxxx"
-# export STORAGE_PASSWORD="xxxxx"
-# export STORAGE_POOL="pool"
+export STORAGE_TYPE="powerscale"
+export END_POINT="1.1.1.1:8080"
+export SYSTEM_ID="Isilon-System-Name"
+export STORAGE_USER="username"
+export STORAGE_PASSWORD="password"
+export STORAGE_POOL="/ifs/data/csi"
 # Must specify and manually create driver namespace
-# export DRIVER_NAMESPACE="namespace"
+export DRIVER_NAMESPACE="isilon"
 
 # The following are for creating PFlex secret/storage class
 # do not include "https://" in the endpoint
-export PFLEX_USER=""
-export PFLEX_PASS=""
-export PFLEX_SYSTEMID=""
-export PFLEX_ENDPOINT=""
-export PFLEX_MDM=""
-export PFLEX_AUTH_ENDPOINT=""  
-export PFLEX_POOL=""
+export PFLEX_USER="username"
+export PFLEX_PASS="password"
+export PFLEX_SYSTEMID="00990099ddcc"
+export PFLEX_ENDPOINT="10.1.1.1"
+export PFLEX_MDM="10.0.0.1,10.0.0.2"
+export PFLEX_AUTH_ENDPOINT="localhost:9401"  
+export PFLEX_POOL="pool1"
 
 # The following are for creating PScale secret/storage class
 # do not include "https://" in the endpoint
-export PSCALE_CLUSTER=""
-export PSCALE_USER=""
-export PSCALE_PASS=""
-export PSCALE_ENDPOINT=""
-export PSCALE_AUTH_ENDPOINT=""
-export PSCALE_AUTH_PORT=""
+export PSCALE_CLUSTER="Isilon-System-Name"
+export PSCALE_USER="username"
+export PSCALE_PASS="password"
+export PSCALE_ENDPOINT="1.1.1.1"
+export PSCALE_AUTH_ENDPOINT="localhost"
+export PSCALE_AUTH_PORT="9400"
 
 # The following are for testing AM
 export VOL_NS=wordpress
 export RES_NS=res-wordpress
 export AM_NS=test-vxflexos
-export BACKEND_STORAGE_URL=""
-export BUCKET_NAME=""
-export ALT_BUCKET_NAME="" 
+export BACKEND_STORAGE_URL="10.0.0.4:32000"
+export BUCKET_NAME="my-bucket"
+export ALT_BUCKET_NAME="alt-bucket" 
 # Be sure to escape / with \
-export AM_CONTROLLER_IMAGE=""
-export AM_PLUGIN_IMAGE=""
+export AM_CONTROLLER_IMAGE="dellemc/csm-application-mobility-controller:nightly"
+export AM_PLUGIN_IMAGE="dellemc/csm-application-mobility-velero-plugin:nightly"
