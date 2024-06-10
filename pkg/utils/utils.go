ba066e05b999d1a1f010f1e8eadc1b84830a2240
meggm <meghana.gm@dell.com>
2024-06-10 12:28:40 +0530

    Remove role/rolebindings for csm objects

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -42,7 +42,6 @@ import (
 	t1 "k8s.io/apimachinery/pkg/types"
 	confv1 "k8s.io/client-go/applyconfigurations/apps/v1"
 	acorev1 "k8s.io/client-go/applyconfigurations/core/v1"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/yaml"
 
