91a123891aff29a2a62c2b5956e905a86debf854
Christian Coffield <christian_coffield@dell.com>
2024-06-11 19:22:26 +0530

    changes to support brownfield onboarding to ANK8s (#588)
    * changes to support brownfield onboarding to ANK8s
    
    * code refactoring
    
    * remove commented code
    
    * remove commented code
    
    * remove commented code
    
    * remove commented code
    
    * remove commented code
    
    * Fixed lint issues
    
    * Fixed lint issues
    
    * Added UT tescases
    
    * Removing the failed testcases
    
    * add new unit test case
    
    * add new unit test case
    
    * refactor the code
    
    * refactor the code
    
    * refactor the code
    
    * refactor the code
    
    * refactor the code
    
    * lint-fix
    
    * lint-fix
    
    * Fixed lint issues
    
    Signed-off-by: Harish P <harish_p4@dell.com>
    
    * Fixed lint issues
    
    Signed-off-by: Harish P <harish_p4@dell.com>
    
    ---------
    
    Signed-off-by: Harish P <harish_p4@dell.com>
    Co-authored-by: Harish P <harish_p4@dell.com>
    Co-authored-by: meggm <meghana.gm@dell.com>

--- controllers/csm_controller.go
+++ controllers/csm_controller.go
@@ -879,6 +879,11 @@ func (r *ContainerStorageModuleReconciler) SyncCSM(ctx context.Context, cr csmv1
 		}
 
 	}
+	// If dell connectivity client is deployed, create role/rolebindings in the csm namespaces
+	if err = utils.CheckAccAndCreateRbac(ctx, operatorConfig, ctrlClient); err != nil {
+		return err
+	}
+
 	return nil
 }
 
