a5962a29b87596a9f8b8eacc9030212899836695
Fernando Alfaro Campos <Fernando.Alfaro_campos@Dell.com>
2024-07-12 13:50:29 -0400

    Fix sanity e2e tests

--- tests/e2e/steps/steps_def.go
+++ tests/e2e/steps/steps_def.go
@@ -1112,13 +1112,13 @@ func (step *Step) configureAuthorizationProxyServer(res Resource, driver string,
 	}
 
 	for key := range mapValues {
-		err := replaceInFile(os.Getenv(mapValues[key]), key, "testfiles/authorization-templates/csm-authorization_storage.yaml")
+		err := replaceInFile(key, os.Getenv(mapValues[key]), "testfiles/authorization-templates/csm-authorization_storage.yaml")
 		if err != nil {
 			return err
 		}
 	}
 	cmd := exec.Command("kubectl", "apply",
-		"-f", "testfiles/authorization-templates/csm-authorization_v1_storage.yaml",
+		"-f", "testfiles/authorization-templates/csm-authorization_storage.yaml",
 	)
 	fmt.Println("=== Storage === \n", cmd.String())
 	b, err = cmd.CombinedOutput()
@@ -1134,13 +1134,13 @@ func (step *Step) configureAuthorizationProxyServer(res Resource, driver string,
 	}
 
 	for key := range mapValues {
-		err := replaceInFile(os.Getenv(mapValues[key]), key, "testfiles/authorization-templates/csm-authorization_csmtenant.yaml")
+		err := replaceInFile(key, os.Getenv(mapValues[key]), "testfiles/authorization-templates/csm-authorization_csmtenant.yaml")
 		if err != nil {
 			return err
 		}
 	}
 	cmd = exec.Command("kubectl", "apply",
-		"-f", "testfiles/authorization-templates/csm-authorization_v1_csmtenant.yaml",
+		"-f", "testfiles/authorization-templates/csm-authorization_csmtenant.yaml",
 	)
 	b, err = cmd.CombinedOutput()
 	fmt.Println("=== Tenant === \n", cmd.String())
@@ -1157,13 +1157,13 @@ func (step *Step) configureAuthorizationProxyServer(res Resource, driver string,
 	}
 
 	for key := range mapValues {
-		err := replaceInFile(os.Getenv(mapValues[key]), key, "testfiles/authorization-templates/csm-authorization_csmrole.yaml")
+		err := replaceInFile(key, os.Getenv(mapValues[key]), "testfiles/authorization-templates/csm-authorization_csmrole.yaml")
 		if err != nil {
 			return err
 		}
 	}
 	cmd = exec.Command("kubectl", "apply",
-		"-f", "testfiles/authorization-templates/csm-authorization_v1_csmrole.yaml",
+		"-f", "testfiles/authorization-templates/csm-authorization_csmrole.yaml",
 	)
 
 	fmt.Println("=== Role === \n", cmd.String())
