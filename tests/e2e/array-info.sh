d20cf402744dd8d45125a6526431363ccad5bc5a
Alik Saring <alik.saring@dell.com>
2024-07-15 16:15:05 -0400

    Cages to correct broken authorization e2e for operator

--- tests/e2e/array-info.sh
+++ tests/e2e/array-info.sh
@@ -1,5 +1,5 @@
 # Copyright © 2022-2024 Dell Inc. or its subsidiaries. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,7 +13,7 @@
 # USER MODIFICATION REQUIRED: must supply address of Authorization Proxy Server
 # Since this e2e exposes the Proxy Server via NodePort, you can use a cluster node IP
 export PROXY_HOST="csm-authorization.com"
-
+export DELLCTL="/usr/local/bin/dellctl"
 # The following are Authorization Proxy Server specific:
 # Must supply storage array details
 # Storage type examples - powerscale, powerflex, powermax
@@ -22,9 +22,14 @@ export END_POINT="1.1.1.1:8080"
 export SYSTEM_ID="Isilon-System-Name"
 export STORAGE_USER="username"
 export STORAGE_PASSWORD="password"
-export STORAGE_POOL="/ifs/data/csi"
+# provide vault path example "storage\/powerscale" escape / with \
+export VAULT_STORAGE_PATH="vault_path"
+# for powerscale isipath escape "/" in the path example "\/ifs\/data\/csi"
+export STORAGE_POOL_PATH="pool1"
 # Must specify and manually create driver namespace
 export DRIVER_NAMESPACE="isilon"
+export TENANT_ROLES="role1"
+export TENANT_PREFIX="tn1"
 
 # The following are for creating PFlex secret/storage class
 # do not include "https://" in the endpoint
@@ -33,7 +38,7 @@ export PFLEX_PASS="password"
 export PFLEX_SYSTEMID="00990099ddcc"
 export PFLEX_ENDPOINT="10.1.1.1"
 export PFLEX_MDM="10.0.0.1,10.0.0.2"
-export PFLEX_AUTH_ENDPOINT="localhost:9401"  
+export PFLEX_AUTH_ENDPOINT="localhost:9401"
 export PFLEX_POOL="pool1"
 
 # The following are for creating PScale secret/storage class
@@ -51,7 +56,7 @@ export RES_NS=res-wordpress
 export AM_NS=test-vxflexos
 export BACKEND_STORAGE_URL="10.0.0.4:32000"
 export BUCKET_NAME="my-bucket"
-export ALT_BUCKET_NAME="alt-bucket" 
+export ALT_BUCKET_NAME="alt-bucket"
 # Be sure to escape / with \
 export AM_CONTROLLER_IMAGE="dellemc/csm-application-mobility-controller:nightly"
 export AM_PLUGIN_IMAGE="dellemc/csm-application-mobility-velero-plugin:nightly"
