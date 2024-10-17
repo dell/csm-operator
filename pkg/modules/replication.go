ce31e4fdf6b740760ad8049ac9e4745d268e1a5f
nitesh3108 <nitesh_rewatkar@dell.com>
2024-10-17 08:48:12 -0400

    standalone replication configmap

--- pkg/modules/replication.go
+++ pkg/modules/replication.go
@@ -20,6 +20,10 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	t1 "k8s.io/apimachinery/pkg/types"
 
 	csmv1 "github.com/dell/csm-operator/api/v1"
