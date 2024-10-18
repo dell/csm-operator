4871a4554b0e6bdcdcaa675cf03a7177c0202a95
Akshay Saini <akshay.saini@dell.com>
2024-10-18 14:43:10 +0530

    Refactor code and add more test cases

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -18,9 +18,30 @@ import (
 
 	csmv1 "github.com/dell/csm-operator/api/v1"
 	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// TestHasModuleComponent tests HasModuleComponent directly, assuming IsModuleEnabled behaves as expected
+// createCR is a helper function to create ContainerStorageModule object
+func createCR(driverType csmv1.DriverType, moduleType csmv1.ModuleType, moduleEnabled bool, components []csmv1.ContainerTemplate) *csmv1.ContainerStorageModule {
+	return &csmv1.ContainerStorageModule{
+		Spec: csmv1.ContainerStorageModuleSpec{
+			Driver: csmv1.Driver{
+				CSIDriverType: driverType,
+			},
+			Modules: []csmv1.Module{
+				{
+					Name:       moduleType,
+					Enabled:    moduleEnabled,
+					Components: components,
+				},
+			},
+		},
+	}
+}
+
 func TestHasModuleComponent(t *testing.T) {
 	tests := []struct {
 		name           string
@@ -30,56 +51,27 @@ func TestHasModuleComponent(t *testing.T) {
 		expectedResult bool
 	}{
 		{
-			name: "Module does not exist",
-			instance: csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Replication,
-							Enabled: true,
-						},
-					},
-				},
-			},
+			name:           "Module does not exist",
+			instance:       *createCR(csmv1.PowerFlex, csmv1.Replication, true, nil),
 			mod:            csmv1.Observability,
 			componentType:  "metrics-powerflex",
 			expectedResult: false,
 		},
 		{
 			name: "Module exist and component does not exist",
-			instance: csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Observability,
-							Enabled: false,
-							Components: []csmv1.ContainerTemplate{
-								{Name: "topology"},
-							},
-						},
-					},
-				},
-			},
+			instance: *createCR(csmv1.PowerFlex, csmv1.Observability, false, []csmv1.ContainerTemplate{
+				{Name: "topology"},
+			}),
 			mod:            csmv1.Observability,
 			componentType:  "metrics-powerflex",
 			expectedResult: false,
 		},
 		{
 			name: "Module exist and component exists",
-			instance: csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Observability,
-							Enabled: false,
-							Components: []csmv1.ContainerTemplate{
-								{Name: "metrics-powerflex"},
-								{Name: "topology"},
-							},
-						},
-					},
-				},
-			},
+			instance: *createCR(csmv1.PowerFlex, csmv1.Observability, false, []csmv1.ContainerTemplate{
+				{Name: "metrics-powerflex"},
+				{Name: "topology"},
+			}),
 			mod:            csmv1.Observability,
 			componentType:  "metrics-powerflex",
 			expectedResult: true,
@@ -110,94 +102,39 @@ func TestAddModuleComponent(t *testing.T) {
 		{
 			name: "Module does not exist",
 			args: args{
-				instance: &csmv1.ContainerStorageModule{
-					Spec: csmv1.ContainerStorageModuleSpec{
-						Modules: []csmv1.Module{
-							{
-								Name:    csmv1.Replication,
-								Enabled: false,
-							},
-						},
-					},
-				},
+				instance:  createCR(csmv1.PowerFlex, csmv1.Replication, false, nil),
 				mod:       csmv1.Observability,
 				component: csmv1.ContainerTemplate{Name: "topology"},
 			},
-			want: &csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Replication,
-							Enabled: false,
-						},
-					},
-				},
-			},
+			want: createCR(csmv1.PowerFlex, csmv1.Replication, false, nil),
 		},
 		{
 			name: "Module exists and component is empty",
 			args: args{
-				instance: &csmv1.ContainerStorageModule{
-					Spec: csmv1.ContainerStorageModuleSpec{
-						Modules: []csmv1.Module{
-							{
-								Name:    csmv1.Observability,
-								Enabled: false,
-							},
-						},
-					},
-				},
+				instance:  createCR(csmv1.PowerFlex, csmv1.Observability, false, nil),
 				mod:       csmv1.Observability,
 				component: csmv1.ContainerTemplate{Name: "topology"},
 			},
-			want: &csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Observability,
-							Enabled: false,
-							Components: []csmv1.ContainerTemplate{
-								{Name: "topology"}},
-						},
-					},
-				},
-			},
+			want: createCR(csmv1.PowerFlex, csmv1.Observability, false, []csmv1.ContainerTemplate{
+				{Name: "topology"},
+			}),
 		},
 		{
 			name: "Module exists and component is not empty",
 			args: args{
-				instance: &csmv1.ContainerStorageModule{
-					Spec: csmv1.ContainerStorageModuleSpec{
-						Modules: []csmv1.Module{
-							{
-								Name:    csmv1.Observability,
-								Enabled: true,
-								Components: []csmv1.ContainerTemplate{
-									{Name: "metrics-powerflex"},
-								},
-							},
-						},
-					},
-				},
+				instance: createCR(csmv1.PowerFlex, csmv1.Observability, true, []csmv1.ContainerTemplate{
+					{Name: "metrics-powerflex"},
+				}),
 				mod:       csmv1.Observability,
 				component: csmv1.ContainerTemplate{Name: "topology"},
 			},
-			want: &csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Observability,
-							Enabled: true,
-							Components: []csmv1.ContainerTemplate{
-								{Name: "metrics-powerflex"},
-								{Name: "topology"},
-							},
-						},
-					},
-				},
-			},
+			want: createCR(csmv1.PowerFlex, csmv1.Observability, true, []csmv1.ContainerTemplate{
+				{Name: "metrics-powerflex"},
+				{Name: "topology"},
+			}),
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			AddModuleComponent(tt.args.instance, tt.args.mod, tt.args.component)
@@ -217,16 +154,6 @@ func TestLoadDefaultComponents(t *testing.T) {
 	*enabled = true
 	disabled := new(bool)
 	*disabled = false
-
-	// Helper function to create ContainerStorageModule object
-	createCR := func(driverType csmv1.DriverType, moduleType csmv1.ModuleType, moduleEnabled bool, components []csmv1.ContainerTemplate) *csmv1.ContainerStorageModule {
-		return &csmv1.ContainerStorageModule{
-			Spec: csmv1.ContainerStorageModuleSpec{
-				Driver:  csmv1.Driver{CSIDriverType: driverType},
-				Modules: []csmv1.Module{{Name: moduleType, Enabled: moduleEnabled, Components: components}},
-			},
-		}
-	}
 
 	type args struct {
 		ctx context.Context
@@ -341,6 +268,162 @@ func TestLoadDefaultComponents(t *testing.T) {
 				assert.Error(t, err)
 			}
 			assert.Equal(t, tt.want, tt.args.cr)
+		})
+	}
+}
+
+func TestSetContainerImage(t *testing.T) {
+	type args struct {
+		objects        []crclient.Object
+		deploymentName string
+		containerName  string
+		image          string
+		want           *corev1.Container
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Test setting image for a valid deployment and container",
+			args: args{
+				objects: []crclient.Object{
+					&appsv1.Deployment{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "my-deployment",
+						},
+						Spec: appsv1.DeploymentSpec{
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:  "my-container",
+											Image: "old-image",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				deploymentName: "my-deployment",
+				containerName:  "my-container",
+				image:          "new-image",
+				want: &corev1.Container{
+					Name:  "my-container",
+					Image: "new-image",
+				},
+			},
+		},
+		{
+			name: "Test setting image for a non-existing deployment",
+			args: args{
+				objects: []crclient.Object{
+					&appsv1.Deployment{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "my-deployment",
+						},
+						Spec: appsv1.DeploymentSpec{
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:  "my-container",
+											Image: "old-image",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				deploymentName: "non-existing-deployment",
+				containerName:  "my-container",
+				image:          "new-image",
+				want:           nil,
+			},
+		},
+		{
+			name: "Test setting image for a non-existing container",
+			args: args{
+				objects: []crclient.Object{
+					&appsv1.Deployment{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "my-deployment",
+						},
+						Spec: appsv1.DeploymentSpec{
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:  "my-container",
+											Image: "old-image",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				deploymentName: "my-deployment",
+				containerName:  "non-existing-container",
+				image:          "new-image",
+				want:           nil,
+			},
+		},
+		{
+			name: "Test setting image for a deployment with no containers",
+			args: args{
+				objects: []crclient.Object{
+					&appsv1.Deployment{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "my-deployment",
+						},
+						Spec: appsv1.DeploymentSpec{
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{},
+								},
+							},
+						},
+					},
+				},
+				deploymentName: "my-deployment",
+				containerName:  "my-container",
+				image:          "new-image",
+				want:           nil,
+			},
+		},
+		{
+			name: "Test setting image for a deployment with no containers and empty objects slice",
+			args: args{
+				objects:        []crclient.Object{},
+				deploymentName: "my-deployment",
+				containerName:  "my-container",
+				image:          "new-image",
+				want:           nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SetContainerImage(tt.args.objects, tt.args.deploymentName, tt.args.containerName, tt.args.image)
+
+			// Find the deployment and container in the objects
+			var container *corev1.Container
+			for _, object := range tt.args.objects {
+				if deployment, ok := object.(*appsv1.Deployment); ok && deployment.Name == tt.args.deploymentName {
+					for _, c := range deployment.Spec.Template.Spec.Containers {
+						if c.Name == tt.args.containerName {
+							container = &c
+							break
+						}
+					}
+					break
+				}
+			}
+
+			assert.Equal(t, tt.args.want, container)
 		})
 	}
 }
