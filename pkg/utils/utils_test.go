61fa9bc4527c98ab27b4ef0075a49e1a04c29f55
Akshay Saini <akshay.saini@dell.com>
2024-10-18 10:05:18 +0530

    Refactor code and add more test cases

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -14,7 +14,6 @@ package utils
 
 import (
 	"context"
-	"reflect"
 	"testing"
 
 	csmv1 "github.com/dell/csm-operator/api/v1"
@@ -208,12 +207,27 @@ func TestAddModuleComponent(t *testing.T) {
 }
 
 func TestLoadDefaultComponents(t *testing.T) {
-	incorrectOp := OperatorConfig{
+	invalidOp := OperatorConfig{
 		ConfigDirectory: "invalid/path",
 	}
-	correctOp := OperatorConfig{
+	validOp := OperatorConfig{
 		ConfigDirectory: "../../operatorconfig",
 	}
+	enabled := new(bool)
+	*enabled = true
+	disabled := new(bool)
+	*disabled = false
+
+	// Helper function to create ContainerStorageModule object
+	createCR := func(driverType csmv1.DriverType, moduleType csmv1.ModuleType, moduleEnabled bool, components []csmv1.ContainerTemplate) *csmv1.ContainerStorageModule {
+		return &csmv1.ContainerStorageModule{
+			Spec: csmv1.ContainerStorageModuleSpec{
+				Driver:  csmv1.Driver{CSIDriverType: driverType},
+				Modules: []csmv1.Module{{Name: moduleType, Enabled: moduleEnabled, Components: components}},
+			},
+		}
+	}
+
 	type args struct {
 		ctx context.Context
 		cr  *csmv1.ContainerStorageModule
@@ -229,72 +243,104 @@ func TestLoadDefaultComponents(t *testing.T) {
 			name: "Observability module does not exist",
 			args: args{
 				ctx: context.Background(),
-				cr: &csmv1.ContainerStorageModule{
-					Spec: csmv1.ContainerStorageModuleSpec{
-						Modules: []csmv1.Module{
-							{
-								Name:    csmv1.Replication,
-								Enabled: true,
-							},
-						},
-					},
-				},
-				op: correctOp,
+				cr:  createCR(csmv1.PowerFlex, csmv1.Replication, true, nil),
+				op:  validOp,
 			},
-			want: &csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Replication,
-							Enabled: true,
-						},
-					},
-				},
-			},
+			want:    createCR(csmv1.PowerFlex, csmv1.Replication, true, nil),
 			wantErr: false,
 		},
 		{
 			name: "Default components not found",
 			args: args{
 				ctx: context.Background(),
-				cr: &csmv1.ContainerStorageModule{
-					Spec: csmv1.ContainerStorageModuleSpec{
-						Modules: []csmv1.Module{
-							{
-								Name:    csmv1.Observability,
-								Enabled: true,
-							},
-						},
-					},
-				},
-				op: incorrectOp,
+				cr:  createCR(csmv1.PowerFlex, csmv1.Observability, true, nil),
+				op:  invalidOp,
 			},
-			want: &csmv1.ContainerStorageModule{
-				Spec: csmv1.ContainerStorageModuleSpec{
-					Modules: []csmv1.Module{
-						{
-							Name:    csmv1.Observability,
-							Enabled: true,
-						},
-					},
-				},
-			},
+			want:    createCR(csmv1.PowerFlex, csmv1.Observability, true, nil),
 			wantErr: true,
 		},
-		// ... other test cases ...
+		{
+			name: "Module disabled and components empty",
+			args: args{
+				ctx: context.Background(),
+				cr:  createCR(csmv1.PowerScale, csmv1.Observability, false, nil),
+				op:  validOp,
+			},
+			want: createCR(csmv1.PowerScale, csmv1.Observability, false, []csmv1.ContainerTemplate{
+				{Name: "topology", Enabled: enabled},
+				{Name: "otel-collector", Enabled: enabled},
+				{Name: "cert-manager", Enabled: disabled},
+				{Name: "metrics-powerscale", Enabled: enabled},
+			}),
+			wantErr: false,
+		},
+		{
+			name: "Module disabled and topology component missing",
+			args: args{
+				ctx: context.Background(),
+				cr: createCR(csmv1.PowerFlex, csmv1.Observability, false, []csmv1.ContainerTemplate{
+					{Name: "otel-collector", Enabled: enabled},
+					{Name: "metrics-powerflex", Enabled: enabled},
+				}),
+				op: validOp,
+			},
+			want: createCR(csmv1.PowerFlex, csmv1.Observability, false, []csmv1.ContainerTemplate{
+				{Name: "otel-collector", Enabled: enabled},
+				{Name: "metrics-powerflex", Enabled: enabled},
+				{Name: "topology", Enabled: enabled},
+				{Name: "cert-manager", Enabled: disabled},
+			}),
+			wantErr: false,
+		},
+		{
+			name: "Module enabled and cert-manager component missing",
+			args: args{
+				ctx: context.Background(),
+				cr: createCR(csmv1.PowerFlex, csmv1.Observability, true, []csmv1.ContainerTemplate{
+					{Name: "topology", Enabled: enabled},
+					{Name: "otel-collector", Enabled: enabled},
+					{Name: "metrics-powerflex", Enabled: enabled},
+				}),
+				op: validOp,
+			},
+			want: createCR(csmv1.PowerFlex, csmv1.Observability, true, []csmv1.ContainerTemplate{
+				{Name: "topology", Enabled: enabled},
+				{Name: "otel-collector", Enabled: enabled},
+				{Name: "metrics-powerflex", Enabled: enabled},
+				{Name: "cert-manager", Enabled: disabled},
+			}),
+			wantErr: false,
+		},
+		{
+			name: "Module disabled and all components exist",
+			args: args{
+				ctx: context.Background(),
+				cr: createCR(csmv1.PowerFlex, csmv1.Observability, false, []csmv1.ContainerTemplate{
+					{Name: "topology", Enabled: enabled},
+					{Name: "otel-collector", Enabled: enabled},
+					{Name: "cert-manager", Enabled: disabled},
+					{Name: "metrics-powerflex", Enabled: enabled},
+				}),
+				op: validOp,
+			},
+			want: createCR(csmv1.PowerFlex, csmv1.Observability, false, []csmv1.ContainerTemplate{
+				{Name: "topology", Enabled: enabled},
+				{Name: "otel-collector", Enabled: enabled},
+				{Name: "cert-manager", Enabled: disabled},
+				{Name: "metrics-powerflex", Enabled: enabled},
+			}),
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := LoadDefaultComponents(tt.args.ctx, tt.args.cr, tt.args.op)
 
-			if (err != nil) != tt.wantErr {
-				t.Errorf("LoadDefaultComponents() error = %v, wantErr %v", err, tt.wantErr)
+			if tt.wantErr {
+				assert.Error(t, err)
 			}
-
-			if !reflect.DeepEqual(tt.args.cr, tt.want) {
-				t.Errorf("LoadDefaultComponents() got = %v, want %v", tt.args.cr, tt.want)
-			}
+			assert.Equal(t, tt.want, tt.args.cr)
 		})
 	}
 }
