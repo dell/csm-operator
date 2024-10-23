2f7f7dfb1fb668cbc06c86572466bdffac2d2c53
meghana_gm <meghana.gm@dell.com>
2024-10-23 15:05:17 +0530

    Auth Module fix for minimal manifest file
    Signed-off-by: meghana_gm <meghana.gm@dell.com>

--- pkg/modules/authorization.go
+++ pkg/modules/authorization.go
@@ -299,25 +299,25 @@ func getAuthApplyCR(cr csmv1.ContainerStorageModule, op utils.OperatorConfig) (*
 		return nil, nil, err
 	}
 
-	// If there are no components, create one
-	if len(authModule.Components) == 0 {
-		authModule.Components = []csmv1.ContainerTemplate{
-			{
-				Name: "karavi-authorization-proxy",
-				Envs: []corev1.EnvVar{
-					{
-						Name:  "PROXY_HOST",
-						Value: "csm-authorization.com",
-					},
-					{
-						Name:  "SKIP_CERTIFICATE_VALIDATION",
-						Value: "true",
-					},
-				},
-			},
+	for i, component := range authModule.Components {
+		if component.Name == "karavi-authorization-proxy" {
+			skipcertFound := false
+			for _, env := range authModule.Components[i].Envs {
+				if env.Name == "SKIP_CERTIFICATE_VALIDATION" {
+					skipcertFound = true
+					break
+				}
+			}
+			// If SKIP_CERTIFICATE_VALIDATION is not found, add it
+			if !skipcertFound {
+				authModule.Components[i].Envs = append(authModule.Components[i].Envs, corev1.EnvVar{
+					Name:  "SKIP_CERTIFICATE_VALIDATION",
+					Value: "true",
+				})
+			}
 		}
-	}
 
+	}
 	container.Env = utils.ReplaceAllApplyCustomEnvs(container.Env, authModule.Components[0].Envs, authModule.Components[0].Envs)
 
 	skipCertValid := false
