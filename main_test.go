064a8b450a2bb066c731e92c6605d0c0bfed854b
Christian Coffield <christian_coffield@dell.com>
2025-02-04 11:25:39 -0500

    added trevor's semver testing

--- main_test.go
+++ main_test.go
@@ -4,14 +4,21 @@ import (
 	"testing"
 
 	"github.com/dell/csm-operator/pkg/logger"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrintVersion(t *testing.T) {
 	_, log := logger.GetNewContextWithLogger("main")
-
+	// TODO: Hook onto the output and verify that it matches the expected
 	printVersion(log)
 }
 
-func TestMain(t *testing.T) {
-	//main()
+func TestGetOperatorConfig(t *testing.T) {
+	_, log := logger.GetNewContextWithLogger("main")
+
+	testConfig := getOperatorConfig(log)
+	assert.NotNil(t, testConfig)
+
+	// TODO: error test cases
+
 }
