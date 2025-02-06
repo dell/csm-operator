6190f950184331ee76ab64eebfd740b56db57e4c
Aaron Tye <tyeaaron@gmail.com>
2025-02-06 17:02:20 +0000

    finish logger

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -23,11 +23,10 @@ func TestGetLogger(t *testing.T) {
 	}
 }
 
-/*
 func TestGetNewContextWithLogger(t *testing.T) {
 	// Test case: No logger in context
 	ctx := context.Background()
-	newCtx, l := GetNewContextWithLogger(ctx, "test")
+	newCtx, l := GetNewContextWithLogger("test")
 	if l == nil {
 		t.Error("Expected non-nil logger, but got nil")
 	}
@@ -37,7 +36,7 @@ func TestGetNewContextWithLogger(t *testing.T) {
 
 	// Test case: Logger in context
 	ctx = context.WithValue(ctx, loggerKey{}, zap.NewNop())
-	newCtx, l = GetNewContextWithLogger(ctx, "test")
+	newCtx, l = GetNewContextWithLogger("test")
 	if l == nil {
 		t.Error("Expected non-nil logger, but got nil")
 	}
@@ -45,65 +44,3 @@ func TestGetNewContextWithLogger(t *testing.T) {
 		t.Error("Expected new context, but got same context")
 	}
 }
-
-func TestNewContextWithLogger(t *testing.T) {
-	// Test case: No logger in context
-	ctx := context.Background()
-	newCtx := NewContextWithLogger(ctx, "test")
-	if ctx == newCtx {
-		t.Error("Expected new context, but got same context")
-	}
-
-	// Test case: Logger in context
-	ctx = context.WithValue(ctx, loggerKey{}, zap.NewNop())
-	newCtx = NewContextWithLogger(ctx, "test")
-	if ctx == newCtx {
-		t.Error("Expected new context, but got same context")
-	}
-}
-
-func TestWithFields(t *testing.T) {
-	// Test case: No logger in context
-	ctx := context.Background()
-	newCtx := WithFields(ctx, zap.String("key", "value"))
-	if ctx == newCtx {
-		t.Error("Expected new context, but got same context")
-	}
-
-	// Test case: Logger in context
-	ctx = context.WithValue(ctx, loggerKey{}, zap.NewNop())
-	newCtx = WithFields(ctx, zap.String("key", "value"))
-	if ctx == newCtx {
-		t.Error("Expected new context, but got same context")
-	}
-}
-
-func TestNewLogger(t *testing.T) {
-	// Test case: Production log level
-	os.Setenv("LOGGER_LEVEL", "PRODUCTION")
-	l, err := newLogger()
-	if err != nil {
-		t.Errorf("Expected nil error, but got %v", err)
-	}
-	if l.Core().Enabled(zapcore.DebugLevel) {
-		t.Error("Expected debug level to be disabled, but it is enabled")
-	}
-
-	// Test case: Development log level
-	os.Setenv("LOGGER_LEVEL", "DEVELOPMENT")
-	l, err = newLogger()
-	if err != nil {
-		t.Errorf("Expected nil error, but got %v", err)
-	}
-	if !l.Core().Enabled(zapcore.DebugLevel) {
-		t.Error("Expected debug level to be enabled, but it is disabled")
-	}
-
-	// Test case: Invalid log level
-	os.Setenv("LOGGER_LEVEL", "INVALID")
-	l, err = newLogger()
-	if err == nil {
-		t.Error("Expected error, but got nil")
-	}
-}
-*/
