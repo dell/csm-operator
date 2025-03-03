91d2aec6a6eb592b2d1669b29e699de8e92902b5
root <shayna_finocchiaro@dell.com>
2025-03-03 20:00:51 +0000

    fix formatting

--- pkg/drivers/powerflex_test.go
+++ pkg/drivers/powerflex_test.go
@@ -154,7 +154,7 @@ var (
 		{
 			name:       "update GOSCALEIO_DEBUG value for Controller",
 			yamlString: "<GOSCALEIO_DEBUG>",
-			cr:        csmv1.ContainerStorageModule{
+			cr: csmv1.ContainerStorageModule{
 				Spec: csmv1.ContainerStorageModuleSpec{
 					Driver: csmv1.Driver{
 						Common: &csmv1.ContainerTemplate{
@@ -165,13 +165,13 @@ var (
 					},
 				},
 			},
-			fileType:   "Controller",
-			expected:   "false",
+			fileType: "Controller",
+			expected: "false",
 		},
 		{
 			name:       "update GOSCALEIO_DEBUG value for Node",
 			yamlString: "<GOSCALEIO_DEBUG>",
-			cr:        csmv1.ContainerStorageModule{
+			cr: csmv1.ContainerStorageModule{
 				Spec: csmv1.ContainerStorageModuleSpec{
 					Driver: csmv1.Driver{
 						Common: &csmv1.ContainerTemplate{
@@ -182,13 +182,13 @@ var (
 					},
 				},
 			},
-			fileType:   "Node",
-			expected:   "false",
+			fileType: "Node",
+			expected: "false",
 		},
 		{
 			name:       "update GOSCALEIO_SHOWHTTP value for Controller",
 			yamlString: "<GOSCALEIO_SHOWHTTP>",
-			cr:        csmv1.ContainerStorageModule{
+			cr: csmv1.ContainerStorageModule{
 				Spec: csmv1.ContainerStorageModuleSpec{
 					Driver: csmv1.Driver{
 						Common: &csmv1.ContainerTemplate{
@@ -199,13 +199,13 @@ var (
 					},
 				},
 			},
-			fileType:   "Controller",
-			expected:   "true",
+			fileType: "Controller",
+			expected: "true",
 		},
 		{
 			name:       "update GOSCALEIO_SHOWHTTP value for Node",
 			yamlString: "<GOSCALEIO_SHOWHTTP>",
-			cr:        csmv1.ContainerStorageModule{
+			cr: csmv1.ContainerStorageModule{
 				Spec: csmv1.ContainerStorageModuleSpec{
 					Driver: csmv1.Driver{
 						Common: &csmv1.ContainerTemplate{
@@ -216,8 +216,8 @@ var (
 					},
 				},
 			},
-			fileType:   "Node",
-			expected:   "true",
+			fileType: "Node",
+			expected: "true",
 		},
 	}
 )
