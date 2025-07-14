ad24dfd3ced06a9bfbb37c0acbbed25480207dbb
santhoshatdell <Santhosh.Lakshmanan@dell.com>
2025-07-14 22:25:21 +0000

    remove vault from test files

--- pkg/modules/authorization.go
+++ pkg/modules/authorization.go
@@ -806,7 +806,7 @@ func authorizationStorageServiceV2(ctx context.Context, isDeleting bool, cr csmv
 	}
 
 	// conversion to int32 is safe for a value up to 2147483647
-	// #nosec G115gg
+	// #nosec G115
 	deployment := getStorageServiceScaffold(cr.Name, cr.Namespace, image, int32(replicas))
 
 	// SecretProviderClasses is supported from config v2.3.0 (CSM 1.15) onwards
