765c5214cee918294a8875a63864fd9b32553315
Owen Enestvedt <Owen.Enestvedt@Dell.Com>
2025-07-15 15:40:59 +0000

    Allow storage template to use kubernetes secret for creation

--- tests/e2e/steps/steps_runner.go
+++ tests/e2e/steps/steps_runner.go
@@ -80,7 +80,7 @@ func StepRunnerInit(runner *Runner, ctrlClient client.Client, clientSet *kuberne
 	runner.addStep(`^Set up ephemeral volume properties \[([^"]*)\] for \[([^"]*)\]`, step.setupEphemeralVolumeProperties)
 
 	// Configure authorization-proxy-server for [powerflex]
-	runner.addStep(`^Configure authorization-proxy-server for \[([^"]*)\] for CR \[(\d+)\]$`, step.configureAuthorizationProxyServer)
+	runner.addStep(`^Configure authorization-proxy-server for \[([^"]*)\] for CR \[(\d+)\] with storage template \[(\d+)\]$`, step.configureAuthorizationProxyServer)
 	// Authorization Proxy Server V2 additional steps
 	runner.addStep(`^Install Authorization CRDs \[(\d+)\]$`, step.createCustomResourceDefinition)
 	runner.addStep(`^Validate \[([^"]*)\] CRD for Authorization is installed$`, step.validateCustomResourceDefinition)
