432482bfde9c6b0f73fca98fec588b1328a59e31
Gosa <kumar_karthik.gosa@dell.com>
2025-07-17 23:20:09 -0500

    minor change

--- pkg/modules/observability.go
+++ pkg/modules/observability.go
@@ -594,10 +594,10 @@ func getPowerStoreMetricsObjects(op operatorutils.OperatorConfig, cr csmv1.Conta
 					volumeEnabled = env.Value
 				} else if strings.Contains(PstoreVolumeIoPollFrequency, env.Name) {
 					volumePollFrequency = env.Value
-				} else if strings.Contains(PstoreSpacePollFrequency, env.Name) {
-					spacePollFrequency = env.Value
 				} else if strings.Contains(PstoreFileSystemPollFrequency, env.Name) {
 					fsPollFrequency = env.Value
+				} else if strings.Contains(PstoreSpacePollFrequency, env.Name) {
+					spacePollFrequency = env.Value
 				} else if strings.Contains(PstoreArrayPollFrequency, env.Name) {
 					arrayPollFrequency = env.Value
 				} else if strings.Contains(ZipkinURI, env.Name) {
