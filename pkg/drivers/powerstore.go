fd99044c737843e54fc3fa7ee937bdbb04004652
mgandharva <mukesh.gandharva@dell.com>
2025-08-19 09:03:33 -0500

    fix: added minimal e2e

--- pkg/drivers/powerstore.go
+++ pkg/drivers/powerstore.go
@@ -73,6 +73,9 @@ const (
 
 	// PowerStoreNfsExportDirectory - NFS Export Directory
 	PowerStoreNfsExportDirectory = "<X_CSI_NFS_EXPORT_DIRECTORY>"
+
+	// CsiVolPrefix - CSI Volume name Prefix
+	CsiVolPrefix string = "<CSI_VOL_PREFIX>"
 )
 
 // PrecheckPowerStore do input validation
@@ -155,6 +158,7 @@ func ModifyPowerstoreCR(yamlString string, cr csmv1.ContainerStorageModule, file
 	nfsServerPort := "2049"
 	nfsExportDirectory := "/var/lib/dell/nfs"
 	debug := "false"
+	csivolprefix := "csivol"
 	if cr.Spec.Driver.Common != nil {
 		for _, env := range cr.Spec.Driver.Common.Envs {
 			if env.Name == "GOPOWERSTORE_DEBUG" {
@@ -168,6 +172,9 @@ func ModifyPowerstoreCR(yamlString string, cr csmv1.ContainerStorageModule, file
 			}
 			if env.Name == "X_CSI_NFS_EXPORT_DIRECTORY" && env.Value != "" {
 				nfsExportDirectory = env.Value
+			}
+			if env.Name == "X_CSI_VOL_PREFIX" && env.Value != "" {
+				csivolprefix = env.Value
 			}
 		}
 	}
@@ -207,6 +214,7 @@ func ModifyPowerstoreCR(yamlString string, cr csmv1.ContainerStorageModule, file
 		yamlString = strings.ReplaceAll(yamlString, PowerStoreNfsClientPort, nfsClientPort)
 		yamlString = strings.ReplaceAll(yamlString, PowerStoreNfsServerPort, nfsServerPort)
 		yamlString = strings.ReplaceAll(yamlString, PowerStoreNfsExportDirectory, nfsExportDirectory)
+		yamlString = strings.ReplaceAll(yamlString, CsiVolPrefix, csivolprefix)
 	case "Controller":
 		if cr.Spec.Driver.Controller != nil {
 			for _, env := range cr.Spec.Driver.Controller.Envs {
@@ -229,6 +237,7 @@ func ModifyPowerstoreCR(yamlString string, cr csmv1.ContainerStorageModule, file
 		yamlString = strings.ReplaceAll(yamlString, PowerStoreNfsClientPort, nfsClientPort)
 		yamlString = strings.ReplaceAll(yamlString, PowerStoreNfsServerPort, nfsServerPort)
 		yamlString = strings.ReplaceAll(yamlString, PowerStoreNfsExportDirectory, nfsExportDirectory)
+		yamlString = strings.ReplaceAll(yamlString, CsiVolPrefix, csivolprefix)
 	case "CSIDriverSpec":
 		if cr.Spec.Driver.CSIDriverSpec != nil && cr.Spec.Driver.CSIDriverSpec.StorageCapacity {
 			storageCapacity = "true"
