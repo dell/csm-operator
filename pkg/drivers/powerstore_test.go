232938d84b4de4df5b92c36404bffa5a0c1ffb8a
Harish P <harish_p4@dell.com>
2025-09-12 14:21:18 +0000

    UT updates

--- pkg/drivers/powerstore_test.go
+++ pkg/drivers/powerstore_test.go
@@ -119,6 +119,27 @@ var (
 			fileType: "Node",
 		},
 		{
+			name:     "when auth module is disabled",
+			csm:      disableAuthModule(),
+			ct:       powerStoreClient,
+			sec:      powerStoreSecret,
+			fileType: "Node",
+		},
+		{
+			name:     "when node object is nil",
+			csm:      getNilNodeObject(),
+			ct:       powerStoreClient,
+			sec:      powerStoreSecret,
+			fileType: "Node",
+		},
+		{
+			name:     "when node object is not nil and Env. is nil",
+			csm:      getNilEnvObject(),
+			ct:       powerStoreClient,
+			sec:      powerStoreSecret,
+			fileType: "Node",
+		},
+		{
 			name:     "when auth module env. is set to true",
 			csm:      setAuthModuleEnv("true"),
 			ct:       powerStoreClient,
@@ -463,6 +484,30 @@ func enableAuthModule() csmv1.ContainerStorageModule {
 			Enabled: true,
 		},
 	}
+	cr.Spec.Driver.Node.Envs = append(cr.Spec.Driver.Node.Envs, corev1.EnvVar{Name: "X_CSM_AUTH_ENABLED", Value: "true"})
+	return cr
+}
+
+func disableAuthModule() csmv1.ContainerStorageModule {
+	cr := csmForPowerStore("csm")
+	cr.Spec.Modules = []csmv1.Module{
+		{
+			Name:    csmv1.Authorization,
+			Enabled: false,
+		},
+	}
+	return cr
+}
+
+func getNilNodeObject() csmv1.ContainerStorageModule {
+	cr := csmForPowerStore("csm")
+	cr.Spec.Driver.Node = nil
+	return cr
+}
+
+func getNilEnvObject() csmv1.ContainerStorageModule {
+	cr := csmForPowerStore("csm")
+	cr.Spec.Driver.Node.Envs = nil
 	return cr
 }
 
