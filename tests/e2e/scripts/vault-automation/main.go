370c14f5de98210226a13ce04fce33d1c66a02f9
Evgeny Uglov <evgeny.uglov@dell.com>
2025-09-22 10:05:59 -0400

    Escaping config value

--- tests/e2e/scripts/vault-automation/main.go
+++ tests/e2e/scripts/vault-automation/main.go
@@ -721,7 +721,7 @@ func (s *sequence) putVaultSecret(path, username, password string) error {
 func (s *sequence) putVaultConfigSecret(path, config string) error {
 	log.Printf("Writing config secret %s in %s", path, s.name)
 	var b bytes.Buffer
-	vaultCmd := fmt.Sprintf("vault kv put -mount=secret %s configKey=%s", path, config)
+	vaultCmd := fmt.Sprintf("vault kv put -mount=secret %s configKey=\"%s\"", path, config)
 	cmd := exec.Command("kubectl", "exec", s.vaultPodName, "--", "sh", "-c", vaultCmd) // #nosec G204 -- this is a test automation tool
 	cmd.Stdout = &b
 	cmd.Stderr = &b
